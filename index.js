3a5c65debf68218c2ab837c9ce30fe74a7d40798
akenoq <akenoq@yandex.ru>
2018-11-07 04:07:54 +0300

    add /admin

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ let app = express();
 
 app.use(express.static(__dirname + "/static"));
 
+app.get('/admin', function(req, res) {
+    res.sendfile("static/index.html");
+});
+
 app.get('/', function(req, res) {
     res.sendfile("static/index.html");
 });
